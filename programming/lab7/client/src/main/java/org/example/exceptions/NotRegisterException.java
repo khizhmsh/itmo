ff4663f268aecd53d367d7a2222d5995f77d3621
khizhmsh <157143664+khizhmsh@users.noreply.github.com>
2024-06-10 13:49:09 +0300

    Delete programming/lab7/client/src/main directory

--- programming/lab7/client/src/main/java/org/example/exceptions/NotRegisterException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class NotRegisterException extends Exception{
-    public NotRegisterException(){
-        super("Сначала войдите или зарегистрируйтесь");
-    }
-}
--- programming/lab7/client/src/main/java/org/example/exceptions/RootException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class RootException extends Exception {
-    public RootException(String message) {
-        super(message);
-    }
-}
